107e8b30af54b5f50aadf0ff55adc83215f9146c
Teddy Onyango <teddonyango@gmail.com>
2019-02-08 09:40:20 +0300

    Created endpoint for users to get specific potical office

--- app/app/api/v1/routes/officeroutes.py
+++ app/app/api/v1/routes/officeroutes.py
@@ -26,6 +26,13 @@ def create_office():
 										"id": id
 									}]
 								}), 201)
+@endpoint.route('/offices', methods=['GET'])
+def get_office(id):
+    office = OfficeModel.get_specific_office(id)
+    if office:
+        return make_response(jsonify({"status": 200, "data": office}), 200)
+    return make_response(jsonify({"status": 404, "error": "We cant find this office"}), 404)
+
 
 
  
