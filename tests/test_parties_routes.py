f7163e3d1a0aa42aa24e0b566667576e40a82097
Teddy Onyango <teddonyango@gmail.com>
2019-02-09 11:50:51 +0300

    Added tests for editing office

--- tests/test_parties_routes.py
+++ tests/test_parties_routes.py
@@ -20,3 +20,23 @@ class RoutesBaseTest(unittest.TestCase):
         self.invalidparty = {
             "_id": 1
         }
+    def test_view_parties(self):
+        response = self.client.get("api/v1/parties")
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["data"], [])
+        self.assertEqual(result["status"], 200)
+
+    def test_edit_party(self):
+        res = self.client.patch("/api/v1/parties/{}/name".format(0),
+                                    data=json.dumps({
+                                        "name": "New Name"
+                                    }), content_type="application/json")
+        self.assertEqual(res.status_code, 404 )
+    
+    def test_delete_party(self):
+        self.client.post(
+                "api/v1/parties", data=json.dumps(self.partytodelete), content_type="application/json")
+        res = self.client.delete(
+                "/api/v1/parties/{}".format(0), content_type="application/json")
+        self.assertEqual(res.status_code, 404)
