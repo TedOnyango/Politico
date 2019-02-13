ecf1a1ff61c2a17300d5d782d71d1594806e6de5
Teddy Onyango <teddonyango@gmail.com>
2019-02-14 02:11:17 +0300

    Implementing liting and pep8 on my code

---
+++ app/api/v2/models/usermodels.py
@@ -0,0 +1,17 @@
+class User():
+    def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname 
+    self.lastname = lastname
+    self.email = email
+    self.password = password
+
+
+
+def sign_up():
+    return 
+def login():
+    return 
+def vote_for_specific_candidate():
+    return 
+def 
+ 
