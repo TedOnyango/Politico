bb9b01c2bac8c06b8ec6d6505d1af4fb9af90a73
Teddy Onyango <teddonyango@gmail.com>
2019-02-14 01:20:11 +0300

    Implementing liting and pep8 on my code

--- app/api/v1/routes/officeroutes.py
+++ app/api/v1/routes/officeroutes.py
@@ -12,54 +12,46 @@ def create_office():
         name = data['name']
         id = data['id']
     except:
-        return make_response(jsonify({"status": 400,
-									  "error": "Must provide id, name and type"
-									  }), 400)
+        return make_response(jsonify({"status": 400, "error":
+                                      "Must provide id, name and type"}), 400)
 
     newoffice = OfficeModel(name=name, type=type, id=id)
     newoffice.save_office()
 
-    return make_response(jsonify({ "status": 201,
-									"data": [{
-										"type": type,
-										"name": name,
-										"id": id
-									}]
-								}), 201)
+    return make_response(jsonify({"status": 201, "data": [{"type": type,
+                                  "name": name, "id": id}]}), 201)
+
+
 @endpoint.route('/offices', methods=['GET'])
 def get_all_offices():
-    return make_response(jsonify({"status": 200, "data": OfficeModel.view_all_offices()}), 200)
+    OFFICES = OfficeModel.view_all_offices()
+    for office in OFFICES:
+        return make_response(jsonify({"status": 200,
+                                      "data": OFFICES
+                                      }), 200)
+    else:
+        return make_response(jsonify({"status": 404, "error":
+                                      "No offices found"}), 400)
+
 
 @endpoint.route('/offices/<int:id>')
 def get_specific_office(id):
-	office = OfficeModel.get_specific_office(id)
-	if office:
-		return make_response(jsonify({"status": 200,
-										"data": office
-									}), 200)
+    office = OfficeModel.get_specific_office(id)
+    if office:
+        id == "id"
+        return make_response(jsonify({"status": 200, "data": office}), 200)
+    return make_response(jsonify({"status": 404, "error":
+                                  "Office not found"}), 404)
 
-	return make_response(jsonify({"status": 404,
-									"error": "Office not found"
-								}), 404)
 
 @endpoint.route('/offices/<int:id>/name', methods=['PATCH'])
 def edit_office(id):
     data = request.get_json()
     name = data["name"]
-
-    office  = OfficeModel.edit_office(id, name)
+    office = OfficeModel.edit_office(id, name)
     if office:
-        return make_response(jsonify({"status": 200,
-                                  "data": [
-                                      {
-                                          "id": id,
-                                          "name": name
-                                      }
-                                  ]
-                                  }), 200)
+        return make_response(jsonify({"status": 200, "data": [{"id": id,
+                                      "name": name}]}), 200)
     return make_response(jsonify({"status": 404,
                                   "error": "Party not found"
                                   }), 404)
-     
-
- 
